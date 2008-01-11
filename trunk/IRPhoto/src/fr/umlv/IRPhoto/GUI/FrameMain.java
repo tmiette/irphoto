4f60746be9355acdef5889b4648f9e4e08e29beb
sebastienmouret <sebastienmouret@7e640c17-623f-0410-a541-c5c4cac5b4d3>
2008-01-11 13:30:28 +0000

    Initial import.

---
+++ trunk/IRPhoto/src/fr/umlv/IRPhoto/GUI/FrameMain.java
@@ -0,0 +1,30 @@
+package fr.umlv.IRPhoto.GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import fr.umlv.IRPhoto.GUI.panel.FactoryPanel;
+
+public class FrameMain {
+
+	private final JFrame jf;
+
+	private final JPanel panelExplorer;
+
+	public FrameMain() {
+
+		this.jf = new JFrame();
+		this.jf.setTitle("IRPhoto");
+		this.jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.panelExplorer = FactoryPanel.createPanelExplorer();
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				this.panelExplorer, null);
+		this.jf.getContentPane().add(splitPane);
+
+		this.jf.pack();
+		this.jf.setVisible(true);
+	}
+}
